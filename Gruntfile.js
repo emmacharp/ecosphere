82e43e5d6347958f934ff66e386b5da880d3f787
Emma <emma@Emmas-MacBook-Pro.local>
2020-02-13 08:53:44 -0500

    Routes

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,5 @@
 module.exports = function(grunt) {
- var root = '/Users/Emma/Sites/-CONFIG-/grunt/'; 
- require('jit-grunt')(grunt);
- require('time-grunt')(grunt);
-
+ var root = '/Users/Emma/projects/grunt-config/';
  require('load-grunt-config')(grunt, {
     configPath: root + '/tasks',
   config: {
